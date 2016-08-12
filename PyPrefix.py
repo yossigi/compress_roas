80ae3bb691351e52b4c270a67f13a4513e3cf892
Omar Sagga <osagga@outlook.com>
2016-08-12 12:27:30 -0400

    Edited to be able to get data from .cvs

--- PyPrefix.py
+++ PyPrefix.py
@@ -11,18 +11,33 @@ from IPSortedStringTrie import Trie
 def getDict(filename):
     IPdict = dict()
     file = open(filename, 'r')
+    g = file.__iter__()
+    g.next()
     for line in file:
-        line = line[:-1].split(' ')
-        AS = line[1]
-        IP = line[2:]
-        for ip in IP:
-            ip = ip.split('-')
-            prefix = ip[0]
-            try:
-                maxLength = ip[1]
-            except IndexError:
-                maxLength = None
-            IPdict.update(ipReady(prefix, AS, maxLength))
+        line = line[:-1].split(',')
+        #print 'This is the line :', line
+        AS = line[0]
+        IP = line[1]
+        ip = IP.split('-')
+        prefix = ip[0]
+        try:
+            maxLength = ip[1]
+        except IndexError:
+            maxLength = None
+        #print prefix,AS,maxLength
+        IPdict.update(ipReady(prefix, AS, maxLength))
+    # for line in file:
+    #     line = line[:-1].split(',')
+    #     AS = line[1]
+    #     IP = line[2:]
+    #     for ip in IP:
+    #         ip = ip.split('-')
+    #         prefix = ip[0]
+    #         try:
+    #             maxLength = ip[1]
+    #         except IndexError:
+    #             maxLength = None
+    #         IPdict.update(ipReady(prefix, AS, maxLength))
     file.close()
     return IPdict
 
@@ -34,9 +49,15 @@ def ipReady(prefix, AS, maxLength=None):
     AS = int(AS)
     return {key: [maxLength, AS, prefix]}
 
-IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/ip_list.txt"
+IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
 t = Trie(getDict(IPfilename))
 
-print len(t.dec_items())
+before = len(t.dec_items())
 t.combine_items()
-print len(t.dec_items())
+after = len(t.dec_items())
+diff= before - after
+p = (diff/before) * 100.0
+print before
+print after
+print diff
+print p,'%'
