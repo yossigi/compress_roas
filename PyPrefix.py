32a372a49b966d7a38a4f96c7f8e44c70feef08d
Omar Sagga <osagga@outlook.com>
2016-08-12 15:19:10 -0400

    To enable .csv reading

--- PyPrefix.py
+++ PyPrefix.py
@@ -11,33 +11,33 @@ from IPSortedStringTrie import Trie
 def getDict(filename):
     IPdict = dict()
     file = open(filename, 'r')
-    # g = file.__iter__()
-    # g.next()
-    # for line in file:
-    #     line = line[:-1].split(',')
-    #     #print 'This is the line :', line
-    #     AS = line[0]
-    #     IP = line[1]
-    #     ip = IP.split('-')
-    #     prefix = ip[0]
-    #     try:
-    #         maxLength = ip[1]
-    #     except IndexError:
-    #         maxLength = None
-    #     #print prefix,AS,maxLength
-    #     IPdict.update(ipReady(prefix, AS, maxLength))
+    g = file.__iter__()
+    g.next()
     for line in file:
-        line = line[:-1].split(' ')
-        AS = line[1]
-        IP = line[2:]
-        for ip in IP:
-            ip = ip.split('-')
-            prefix = ip[0]
-            try:
-                maxLength = ip[1]
-            except IndexError:
-                maxLength = None
-            IPdict.update(ipReady(prefix, AS, maxLength))
+        line = line[:-1].split(',')
+        #print 'This is the line :', line
+        AS = line[0]
+        IP = line[1]
+        ip = IP.split('-')
+        prefix = ip[0]
+        try:
+            maxLength = ip[1]
+        except IndexError:
+            maxLength = None
+        #print prefix,AS,maxLength
+        IPdict.update(ipReady(prefix, AS, maxLength))
+    # for line in file:
+    #     line = line[:-1].split(' ')
+    #     AS = line[1]
+    #     IP = line[2:]
+    #     for ip in IP:
+    #         ip = ip.split('-')
+    #         prefix = ip[0]
+    #         try:
+    #             maxLength = ip[1]
+    #         except IndexError:
+    #             maxLength = None
+    #         IPdict.update(ipReady(prefix, AS, maxLength))
     file.close()
     return IPdict
 
@@ -49,7 +49,7 @@ def ipReady(prefix, AS, maxLength=None):
     AS = int(AS)
     return {key: [maxLength, AS, prefix]}
 
-IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/ip_list.txt"
+IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
 # IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
 t = Trie(getDict(IPfilename))
 
@@ -60,6 +60,4 @@ diff= len(before) - len(after)
 p = float(diff/float(len(before))) * 100.0
 print len(before)
 print len(after)
-print after
-print diff
 print p,'%'
