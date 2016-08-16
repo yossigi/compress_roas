7237e438fc9fbe02ccdce784b1eeaf02c9db9e4a
Omar Sagga <osagga@outlook.com>
2016-08-16 14:04:18 -0400

    Old version to test with files

--- PyPrefix.py
+++ PyPrefix.py
@@ -7,7 +7,6 @@ Created on Wed Aug 03 12:14:35 2016
 import map_functions as binTools
 from IPSortedStringTrie import Trie
 
-
 def getDict(filename):
     IPdict = dict()
     file = open(filename, 'r')
@@ -41,7 +40,6 @@ def getDict(filename):
     file.close()
     return IPdict
 
-
 def ipReady(prefix, AS, maxLength=None):
     key = binTools.prefix_to_key(prefix)
     if maxLength is None:
@@ -53,8 +51,12 @@ IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
 # IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
 t = Trie(getDict(IPfilename))
 
+
 before = t.dec_items()
+
+#Here I do the minimizing of the ROA's
 t.combine_items()
+
 after = t.dec_items()
 diff= len(before) - len(after)
 p = float(diff/float(len(before))) * 100.0
