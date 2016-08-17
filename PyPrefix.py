a82789260e5fd7e78fddb59632361044f6cff056
Omar Sagga <osagga@outlook.com>
2016-08-17 13:34:13 -0400

    Fixed the multiple AS's problem

--- PyPrefix.py
+++ PyPrefix.py
@@ -7,14 +7,15 @@ Created on Wed Aug 03 12:14:35 2016
 import map_functions as binTools
 from IPSortedStringTrie import Trie
 
-def getDict(filename):
+def getDictCSV(filename):
     IPdict = dict()
+    i = 0
     file = open(filename, 'r')
     g = file.__iter__()
     g.next()
     for line in file:
+        i += 1
         line = line[:-1].split(',')
-        #print 'This is the line :', line
         AS = line[0]
         IP = line[1]
         ip = IP.split('-')
@@ -23,20 +24,41 @@ def getDict(filename):
             maxLength = ip[1]
         except IndexError:
             maxLength = None
-        #print prefix,AS,maxLength
-        IPdict.update(ipReady(prefix, AS, maxLength))
-    # for line in file:
-    #     line = line[:-1].split(' ')
-    #     AS = line[1]
-    #     IP = line[2:]
-    #     for ip in IP:
-    #         ip = ip.split('-')
-    #         prefix = ip[0]
-    #         try:
-    #             maxLength = ip[1]
-    #         except IndexError:
-    #             maxLength = None
-    #         IPdict.update(ipReady(prefix, AS, maxLength))
+
+        key = binTools.prefix_to_key(prefix)
+        if key in IPdict:
+            IPdict[key] = [IPdict[key][0], IPdict[key][1] + [AS], prefix]
+        else:
+            IPdict.update(ipReady(prefix, AS, maxLength))
+
+    file.close()
+    print 'Number of lines is:', i
+    return IPdict
+
+def getDictTXT(filename):
+    IPdict = dict()
+    file = open(filename, 'r')
+    i = 0
+    for line in file:
+        line = line[:-1].split(' ')
+        AS = line[1]
+        IP = line[2:]
+        for ip in IP:
+            i += 1
+            ip = ip.split('-')
+            prefix = ip[0]
+            try:
+                maxLength = ip[1]
+            except IndexError:
+                maxLength = None
+
+            key = binTools.prefix_to_key(prefix)
+            if key in IPdict:
+                IPdict[key] = [IPdict[key][0], IPdict[key][1] + [AS], prefix]
+            else:
+                IPdict.update(ipReady(prefix, AS, maxLength))
+
+    print 'Number of lines is:', i
     file.close()
     return IPdict
 
@@ -45,21 +67,23 @@ def ipReady(prefix, AS, maxLength=None):
     if maxLength is None:
         maxLength = len(key) - 2  # Because the '$' and v number {4,6}
     AS = int(AS)
-    return {key: [maxLength, AS, prefix]}
+    return {key: [maxLength, [AS], prefix]}
 
-IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
-# IPfilename = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
-t = Trie(getDict(IPfilename))
+IPfilenameCSV = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/valid_prefixes_list.csv"
+IPfilenameTXT = "C:\Users\OSAGGA\Documents\ROA_PyTrie\/roa_list.txt"
 
+t = Trie(getDictCSV(IPfilenameCSV))
+#t = Trie(getDictTXT(IPfilenameTXT))
+
+trieSet = t.dec_items()
 
 before = t.dec_items()
-
-#Here I do the minimizing of the ROA's
+# Here I do the minimizing of the ROA's
 t.combine_items()
 
 after = t.dec_items()
-diff= len(before) - len(after)
-p = float(diff/float(len(before))) * 100.0
+diff = len(before) - len(after)
+p = float(diff / float(len(before))) * 100.0
 print len(before)
 print len(after)
-print p,'%'
+print p, '%'
