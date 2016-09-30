1d97fe1fef22448b38c618c52663f6d1f3f19903
Omar Sagga <osagga@bu.edu>
2016-09-30 11:02:11 -0400

    Enhanced version where each AS has its own Trie

--- IPSortedStringTrie.py
+++ IPSortedStringTrie.py
@@ -1,6 +1,5 @@
 from pytrie import SortedStringTrie as trie, NULL, Node as node
-import map_functions as binTools
-import multiprocessing
+
 
 
 class nodeS(node):
@@ -37,72 +36,3 @@ class Trie(trie):
                 del parts[-1]
         node = self._root
         return generator(node)
-
-    # def combine_items(self):
-    #     ''' This function starts the DFS starting from the root of the Trie.'''
-    #     self.dfs_items(self._root)
-
-    def dfs_items(self, node):
-        ''' This function compresses the prefix's '''
-
-        if node is None or not node.children:
-            # When you reach a leaf node, just stop.
-            return
-
-        g = node.children.iteritems()
-
-        # To get pointers on the children nodes.
-        fchild = node.children.get(str(g.next()[0]))
-        try:
-            schild = node.children.get(str(g.next()[0]))
-        # in case a second child doesn't exist.
-        except StopIteration:
-            schild = None
-
-        # The recursive call to reach the end of the Trie.
-        self.dfs_items(fchild)
-        self.dfs_items(schild)
-
-        # Check if the node is an prefix (not just a connecting node) and that 2 children exist with prefix's and value's
-        if node.value is not NULL and fchild.value is not NULL and schild is not None and schild.value is not NULL:
-            # print 'HI!'
-            # To check if the maxLength of the parent is higher or equal to the max(children's  maxLength)
-            if node.value[3] >= maxML([fchild, schild]):
-                pass # No need to change the maxLength in this case.
-            else:
-                # Only update the max length of the parent if it's less than the max of children
-                node.value[3] = minML([fchild, schild])
-            # Only hide a child if the parent's max length is covering the child's max length
-            if node.value[3] >= fchild.value[3]:
-                key = binTools.prefix_to_key(fchild.value[2],fchild.value[1])
-                del self[key]
-            # Only hide a child if the parent's max length is covering the child's max length
-            if node.value[3] >= schild.value[3]:
-                key = binTools.prefix_to_key(schild.value[2],schild.value[1])
-                del self[key]
-
-
-def minML(childList):
-    ''' This method should return the min of the children's maxLength'''
-    numlist = list()
-    for child in childList:
-        numlist += [child.value[3]]  # Add the MaxLength to the list
-    return min(numlist)
-
-def maxML(childList):
-    ''' This method should return the max of the children's maxLength'''
-    numlist = list()
-    for child in childList:
-        numlist += [child.value[3]]  # Add the MaxLength to the list
-    return max(numlist)
-def AS_nodes(self,node):
-    g = node.children.iteritems()
-
-    # To get pointers on the children nodes.
-    fchild = node.children.get(str(g.next()[0]))
-    if fchild
-
-def compress(self):
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    nodes = AS_nodes(self._root)
-    [pool.apply_async( dfs_items, AS ) for AS in nodes]
